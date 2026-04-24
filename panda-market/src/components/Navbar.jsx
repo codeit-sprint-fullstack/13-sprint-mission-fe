31896e56e6ad47d8b399ef8af090022e95dfc55e
seoyoon-k <its.yoon.work@gmail.com>
2026-04-24 10:05:33 +0900

    feat: Footer, Navbar 컴포넌트 작성 및 CSS 연결

--- panda-market/src/components/Navbar.jsx
+++ panda-market/src/components/Navbar.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "../css/Navbar.css";
+import logo from "../assets/logo/logo-panda-market.svg";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="gnb">
+      <div className="gnb-inner">
+        <a href="./">
+          <img className="gnb-logo" src={logo} alt="판다마켓" />
+        </a>
+        <a href="/login" className="gnb-login text-lg-semibold">
+          로그인
+        </a>
+      </div>
+    </header>
+  );
 }

77373b3de5864c7ce83a2a5506108cd8ec628ff3
seoyoon-k <its.yoon.work@gmail.com>
2026-04-26 16:03:11 +0900

    feat: ItemCard 컴포넌트 추가

--- panda-market/src/components/ItemCard.jsx
+++ panda-market/src/components/ItemCard.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import heart from "../assets/icon/heart.svg";
+import heartFill from "../assets/icon/heart-fill.svg";
 
-export default function ItemCard() {
-  return <div>ItemCard</div>;
+export default function ItemCard({ id, image, name, price, favoriteCount }) {
+  const [like, setLike] = useState(false);
+  const isLiked = like ? heart : heartFill;
+  return (
+    <div key={id}>
+      <img src={image} />
+      <div>
+        <div>
+          <h3>{name}</h3>
+          <p>{price}</p>
+        </div>
+        <div>
+          <img onClick={() => setLike((prev) => !prev)} src={isLiked} />
+          <p>{favoriteCount}</p>
+        </div>
+      </div>
+    </div>
+  );
 }

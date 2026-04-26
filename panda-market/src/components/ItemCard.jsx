1f02658aecd2f69d4ca531dc3633e5556bd370e6
seoyoon-k <its.yoon.work@gmail.com>
2026-04-26 16:24:23 +0900

    fix: ItemCard 하트 토글 이미지 오류 수정

--- panda-market/src/components/ItemCard.jsx
+++ panda-market/src/components/ItemCard.jsx
@@ -5,18 +5,20 @@ import heartFill from "../assets/icon/heart-fill.svg";
 
 export default function ItemCard({ id, image, name, price, favoriteCount }) {
   const [like, setLike] = useState(false);
-  const isLiked = like ? heart : heartFill;
+  const isLiked = like ? heartFill : heart;
   return (
-    <div key={id}>
-      <img src={image} />
-      <div>
+    <div className="wrapper" key={id}>
+      <img className="image" src={image} />
+      <div className="container">
+        <h3 className="title">{name}</h3>
+        <p className="price">{price}</p>
         <div>
-          <h3>{name}</h3>
-          <p>{price}</p>
-        </div>
-        <div>
-          <img onClick={() => setLike((prev) => !prev)} src={isLiked} />
-          <p>{favoriteCount}</p>
+          <img
+            className="isLiked"
+            onClick={() => setLike((prev) => !prev)}
+            src={isLiked}
+          />
+          <p className="favoriteCount">{favoriteCount}</p>
         </div>
       </div>
     </div>

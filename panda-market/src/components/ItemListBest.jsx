eec6c7bf8296ec90a032b27f8b3326505cb54d72
seoyoon-k <its.yoon.work@gmail.com>
2026-05-01 17:03:36 +0900

    feat: ItemListBest 컴포넌트 추가

--- panda-market/src/components/ItemListBest.jsx
+++ panda-market/src/components/ItemListBest.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ItemCard from "./ItemCard";
+import styles from "../css/ItemListBest.module.css";
 
 export default function ItemListBest() {
-  return <div>ItemListBest</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch(
+          "https://panda-market-api.vercel.app/products?page=1&pageSize=10&orderBy=favorite",
+        );
+        const data = await response.json();
+        setProducts(data.list);
+      } catch (error) {
+        console.error("error:", error);
+      }
+    }
+    getProducts();
+  }, []);
+
+  return (
+    <div className={styles.list}>
+      {products.map((product) => (
+        <ItemCard
+          key={product.id}
+          image={product.images}
+          name={product.name}
+          price={product.price}
+          favoriteCount={product.favoriteCount}
+        />
+      ))}
+    </div>
+  );
 }

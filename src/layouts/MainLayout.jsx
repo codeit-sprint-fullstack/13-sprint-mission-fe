66b0518fdd763b24f0ec96050e539b332f9b9bed
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-06 14:04:54 +0900

    #37 feat: add requirements mkdn and react router dom

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Header, Footer } from "@/components";
 import styles from "./MainLayout.module.css";
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
   return (
     <div className={styles.layout}>
       <Header />
-      <main className={styles.main}>{children}</main>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
 export { default as MarketPage } from "./market/MarketPage";
+export { default as LoginPage } from "./auth/LoginPage";
+export { default as SignUpPage } from "./auth/SignUpPage";
+export { default as HomePage } from "./home/HomePage";
+export { default as RegisterationPage } from "./registeration/RegisterationPage";

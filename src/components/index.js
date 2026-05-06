c03fc60ca8f535b7ecc88484ea87307645a46ece
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-06 17:23:36 +0900

    #40 feat: create InputBlock component
    - InputBlock 컴포넌트 생성
        - Input에 title와 errorMsg까지 있는 구조
    - Input 컴포넌트 리팩토링 및 기능 추가
        - ...props로 Input의 props 정리
        - prefix와 suffix props추가로 자유자제로 아이콘 추가 가능
        - multiline prop 추가로 textarea 경우 추가
    - 객체 형태의 data를 useState에 넣어 인풋 상황 관리하기
    - 간략한 인풋 체크 로직 추가

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Header } from "./Header/Header";
 export { default as Button } from "./Button/Button";
 export { default as ProductCardList } from "./ProductCardList/ProductCardList";
 export { default as Input } from "./Input/Input";
+export { default as InputBlock } from "./InputBlock/InputBlock";
 export { default as Dropdown } from "./Dropdown/Dropdown";
 export { default as Pagination } from "./Pagination/Pagination";
 export { default as Item } from "./Item/Item";

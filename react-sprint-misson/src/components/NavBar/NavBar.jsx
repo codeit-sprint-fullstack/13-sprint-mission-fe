19795b8a016654b2721671d0b3c69371d87784c4
daeyeong1573 <rlaeodud9983@naver.com>
2026-05-06 19:11:30 +0900

    [refact] header nav 분리 및 페이지에 따른 navbar 보여주기 처리

---
+++ react-sprint-misson/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navContainer}>
+      <NavLink
+        to="/board"
+        className={({ isActive }) =>
+          `${styles.navTxt} ${isActive ? styles.active : ""}`
+        }
+      >
+        자유게시판
+      </NavLink>
+
+      <NavLink
+        to="/items"
+        className={({ isActive }) =>
+          `${styles.navTxt} ${isActive ? styles.active : ""}`
+        }
+      >
+        중고마켓
+      </NavLink>
+    </nav>
+  );
+}
--- react-sprint-misson/src/layouts/Header/Header.jsx
+++ react-sprint-misson/src/layouts/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import logo from "@/assets/logos/logo.svg";
-import styles from "@/styles/Header.module.css";
-import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { Link, useLocation } from "react-router-dom";
+import Navbar from "@/components/NavBar/NavBar";
 
 export default function Header() {
+  const { pathname } = useLocation();
+  const isLanding = pathname === "/";
+
   return (
     <header className={styles.header}>
       <nav className={styles.navBar}>
@@ -12,14 +16,8 @@ export default function Header() {
             <img src={logo} alt="판다마켓 로고" className={styles.logoImage} />
             <span className={styles.logoText}>판다마켓</span>
           </Link>
-          <Link to="/board" className={styles.navLink}>
-            자유게시판
-          </Link>
-          <Link to="/items" className={styles.navLink}>
-            중고마켓
-          </Link>
+          {!isLanding && <Navbar />}
         </div>
-
         <a href="/login" className={styles.loginBtn}>
           로그인
         </a>

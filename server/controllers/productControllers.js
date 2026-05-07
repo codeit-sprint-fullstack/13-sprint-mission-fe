150df4eee9b275f4ecef41e6f5f73ebea9a9ef21
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-07 18:10:34 +0900

    #51 feat: create product apis

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,15 +1,61 @@
+import Product from "../models/Product.js";
+
 export async function getProduct(req, res) {
-  res.send("getProduct 잘 돌아감");
+  const { page = 1, pageSize = 10, orderBy = "recent", keyword } = req.query;
+  //pagination 계산
+  const skip = (Number(page) - 1) * Number(pageSize);
+
+  //일단 find하기
+  let result = Product.find();
+
+  //keyword가 있으면 해당 keyword 포함하는 데이터 리턴
+  if (keyword) {
+    result = result.find({
+      $or: [
+        {
+          name: {
+            $regex: keyword,
+            $options: "i",
+          },
+        },
+        {
+          description: {
+            $regex: keyword,
+            $options: "i",
+          },
+        },
+      ],
+    });
+  }
+
+  //orderBy 있으면 orderBy에 따라 DESC 정렬
+  if (orderBy === "favorite") {
+    result = result.sort({ favoriteCount: -1 });
+  } else {
+    result = result.sort({ createdAt: -1 });
+  }
+
+  //pagination 적용
+  result = await result.skip(skip).limit(Number(pageSize));
+
+  res.status(200).json(result);
 }
 export async function postProduct(req, res) {
-  res.send("postProduct 잘 돌아감");
+  const result = await Product.create(req.body);
+  res.status(201).json(result);
 }
 export async function patchProduct(req, res) {
-  res.send("patchProduct 잘 돌아감");
+  const { productId } = req.params;
+  const result = await Product.findByIdAndUpdate(productId, req.body);
+  res.status(200).json(result);
 }
 export async function deleteProduct(req, res) {
-  res.send("deleteProduct 잘 돌아감");
+  const { productId } = req.params;
+  const result = await Product.findByIdAndDelete(productId);
+  res.status(200).json(result);
 }
 export async function getProductDetail(req, res) {
-  res.send("getProduct 상세 잘 돌아감");
+  const { productId } = req.params;
+  const result = await Product.findById(productId);
+  res.status(200).json(result);
 }

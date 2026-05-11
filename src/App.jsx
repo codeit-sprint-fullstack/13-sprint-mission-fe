9315fcf7cd7f3b1aef8a04b4e992c2bfa3ee3592
seoyoon-k <its.yoon.work@gmail.com>
2026-05-11 11:46:23 +0900

    chore: route 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainLayout from "./layouts/MainLayout.jsx";
-import SubLayout from "./layouts/SubLayout.jsx";
 import Home from "./pages/Home";
 import Items from "./pages/Items";
 import Registration from "./pages/Registration.jsx";
@@ -9,11 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* useLocation 활용해서 레이아웃 하나로 가는게 현 상황에는 낫다 */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<SubLayout />}>
           <Route path="/items" element={<Items />} />
           <Route path="/registration" element={<Registration />} />
         </Route>

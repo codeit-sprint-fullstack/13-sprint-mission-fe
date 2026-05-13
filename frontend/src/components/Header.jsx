7d01a454fb228bfe0c5cbef12b9097159c753c19
KIMJIHUN <kimjihun4118@gmail.com>
2026-05-13 16:49:57 +0900

    스프린트 미션 5 제출

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import pandalogo from "../assets/panda-logo..svg";
+import "../css/Header.css";
+import "../css/root.css";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+   return (
+      <header id="header">
+         <div className="market-logo">
+            <Link to="/">
+               <img
+                  className="logo-mark"
+                  src={pandalogo}
+                  alt="판다마켓 로고 이미지"
+               />
+               <p id="logo-title">판다마켓</p>
+            </Link>
+            <p className="community">자유게시판</p>
+            <NavLink
+               to="/items"
+               className={({ isActive }) =>
+                  isActive ? "second-hands active" : "second-hands"
+               }
+            >
+               중고마켓
+            </NavLink>
+         </div>
+
+         <div className="login-container">
+            <Link to="/login" className="lgn-btn">
+               로그인
+            </Link>
+         </div>
+      </header>
+   );
+}

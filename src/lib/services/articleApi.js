e71c875419c798a16a26adae437b55c8a533797b
damdadaxx <hyojudev@gmail.com>
2026-06-16 17:17:59 +0900

    feat: useValidators 훅 및 articleApi 서비스 함수 작성

--- src/lib/services/articleApi.js
+++ src/lib/services/articleApi.js
@@ -1,4 +1,4 @@
-export async function getAllArticles({
+async function getAllArticles({
   pageSize = 10,
   page = 1,
   order = "recent",
@@ -21,6 +21,65 @@ export async function getAllArticles({
 
     return await response.json();
   } catch (error) {
-    throw new Error(`❌ API Error :`, { cause: error });
+    throw new Error(`❌ 게시글 조회 실패 :`, { cause: error });
   }
 }
+
+async function getArticleById(id) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/${id}`,
+    );
+
+    if (!response.ok) throw new Error(`❌ API 에러! 상태: ${response.status}`);
+    if (response.status === 204) return null;
+
+    return await response.json();
+  } catch (error) {
+    throw new Error(`❌ 게시글 조회 실패 :`, { cause: error });
+  }
+}
+
+async function createArticle(body) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      },
+    );
+
+    if (!response.ok) throw new Error(`❌ API 에러! 상태: ${response.status}`);
+
+    return await response.json();
+  } catch (error) {
+    throw new Error("❌ 게시글 등록 실패", { cause: error });
+  }
+}
+
+async function updateArticle(id, body) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/${id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      },
+    );
+
+    if (!response.ok) throw new Error(`❌ API 에러! 상태: ${response.status}`);
+
+    return await response.json();
+  } catch (error) {
+    throw new Error("❌ 게시글 수정 실패", { cause: error });
+  }
+}
+
+export { getAllArticles, getArticleById, createArticle, updateArticle };

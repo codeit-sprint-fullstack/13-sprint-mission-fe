4ea5ce77553bb3c1674346411f1f116fa8632537
damdadaxx <hyojudev@gmail.com>
2026-06-17 16:16:41 +0900

    refactor: Button, Header, Footer 공통 컴포넌트 수정 - Button: width props 추가 및 클래스네임 정리 - Header, Footer: container 스타일 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,8 +2,9 @@ import { clsx } from "clsx";
 
 export default function Button({
   as: Component = "button",
-  className = "",
   variant = "primary",
+  className = "",
+  width = "",
   children,
   ...props
 }) {
@@ -12,11 +13,12 @@ export default function Button({
       className={clsx(
         "flex justify-center items-center cursor-pointer",
         variant === "primary" &&
-          "h-10.5 py-3 px-5.75 rounded-lg text-[16px]/[calc(26/16)] font-semibold text-gray-100 bg-primary-100 disabled:text-gray-100 disabled:bg-cool-gray-400 disabled:cursor-default",
+          "h-[42px] py-[12px] px-[23px] rounded-[8px] text-[16px]/[calc(26/16)] font-semibold text-gray-100 bg-primary-100 disabled:text-gray-100 disabled:bg-cool-gray-400 disabled:cursor-default",
         variant === "secondary" &&
-          "w-60 p-3 text-[18px]/[calc(26/18)] font-semibold rounded-[40px] text-secondary-50 bg-primary-100 md:w-89.25 md:text-[20px]/[calc(32/20)]",
+          "p-[12px] text-[18px]/[calc(26/18)] font-semibold rounded-[40px] text-secondary-50 bg-primary-100 md:text-[20px]/[calc(32/20)]",
         className,
       )}
+      style={width ? { width: `${width}px` } : undefined}
       {...props}
     >
       {children}
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -16,51 +16,53 @@ export default function Header() {
 
   return (
     <header className='h-[70px]'>
-      <div className='fixed top-0 left-0 right-0 z-header flex items-center justify-between w-full h-[70px] max-w-[1920px] px-[16px] md:px-[24px] xl:px-[200px] mx-auto bg-white border-b border-[#dfdfdf]'>
-        <Link href='/'>
-          <figure className='flex-none'>
-            <Image
-              className='md:hidden'
-              src={MobileLogo}
-              alt='로고 이미지'
-              width={81}
-              height={40}
-              priority
-            />
-            <Image
-              className='hidden md:block'
-              src={Logo}
-              alt='로고 이미지'
-              width={153}
-              height={51}
-              priority
-            />
-          </figure>
-        </Link>
+      <div className='fixed top-0 left-0 right-0 z-header bg-white border-b border-[#dfdfdf]'>
+        <div className='flex items-center justify-between w-full max-w-[1200px] h-[70px] px-[16px] md:px-[24px] mx-auto '>
+          <Link href='/'>
+            <figure className='flex-none'>
+              <Image
+                className='md:hidden'
+                src={MobileLogo}
+                alt='로고 이미지'
+                width={81}
+                height={40}
+                priority
+              />
+              <Image
+                className='hidden md:block'
+                src={Logo}
+                alt='로고 이미지'
+                width={153}
+                height={51}
+                priority
+              />
+            </figure>
+          </Link>
 
-        <nav className='flex-1 h-full px-[12px] md:px-[20px] xl:px-[24px]'>
-          <ul className='flex items-center h-full'>
-            {NAVIGATION.map((nav) => (
-              <li key={nav.link.slice(1)} className='h-full'>
-                <Link
-                  href={nav.link}
-                  className={clsx(
-                    pathname === nav.link
-                      ? "text-primary-100"
-                      : "text-secondary-600",
-                    "flex items-center h-full px-1 md:px-[15px] text-[16px] md:text-[18px] font-bold leading-[1.625] text-center",
-                  )}
-                >
-                  {nav.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+          <nav className='flex-1 h-full px-[12px] md:px-[20px] xl:px-[24px]'>
+            <ul className='flex items-center h-full'>
+              {NAVIGATION.map((nav) => (
+                <li key={nav.link.slice(1)} className='h-full'>
+                  <Link
+                    href={nav.link}
+                    className={clsx(
+                      pathname === nav.link
+                        ? "text-primary-100"
+                        : "text-secondary-600",
+                      "flex items-center h-full px-1 md:px-[15px] text-[16px] md:text-[18px] font-bold leading-[1.625] text-center",
+                    )}
+                  >
+                    {nav.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        <Button as={Link} href='/login'>
-          로그인
-        </Button>
+          <Button as={Link} href='/login'>
+            로그인
+          </Button>
+        </div>
       </div>
     </header>
   );

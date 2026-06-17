8b2c4d11b5949faec41a9e1760946ff44ed8bbb6
ydj-13 <de2013@naver.com>
2026-06-17 09:23:03 +0900

    feat : header 작업

---
+++ src/app/community/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ComuunityPage() {
+  return <div>자유게시판</div>;
+}
---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "판다 마켓",
+  description: "판다마켓-스프린트 미션",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Header></Header>
+        <main className="flex-1">{children}</main>
+        <Footer></Footer>
+      </body>
+    </html>
+  );
+}

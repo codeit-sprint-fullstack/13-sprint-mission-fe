5ea2b9c02a2a76a9d7fd89e540226b2cb9fba8fb
apple <apple@appleui-MacBookAir.local>
2026-06-17 09:53:17 +0900

    게시글 등록 페이지 작업 완료

--- sprint_mission_7/src/api/article.js
+++ sprint_mission_7/src/api/article.js
@@ -1,3 +1,4 @@
+// 1. 게시글 전체 조회
 export const getAllArticles = async (
   searchValue = "",
   pageNum = 1,
@@ -14,3 +15,66 @@ export const getAllArticles = async (
 
   return response.json();
 };
+
+// 2. 게시글 상세 조회
+
+export async function getArticle(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/articles/${id}`);
+    if (!response.ok) throw new Error("게시글을 가져오는데 실패했습니다");
+    return await response.json();
+  } catch (error) {
+    console.error(`getArticle(${id}) 에러:`, error);
+    throw error;
+  }
+}
+
+// 3. 게시글 등록
+export async function createArticle(articleData) {
+  try {
+    const response = await fetch("http://localhost:3000/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(articleData),
+    });
+    if (!response.ok) throw new Error("게시글 등록에 실패했습니다.");
+    return await response.json();
+  } catch (error) {
+    console.error("createArticle 에러:", error);
+    throw error;
+  }
+}
+
+// 4. 게시글 수정
+export async function updateArticle(id, articleData) {
+  try {
+    const response = await fetch(`http://localhost:3000/articles/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(articleData),
+    });
+    if (!response.ok) throw new Error("게시글 수정에 실패했습니다.");
+    return await response.json();
+  } catch (error) {
+    console.error(`updateArticle(${id}) 에러:`, error);
+    throw error;
+  }
+}
+
+// 5. 게시글 삭제
+export async function deleteArticle(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/articles/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) throw new Error("게시글 삭제에 실패했습니다.");
+    return await response.json();
+  } catch (error) {
+    console.error(`deleteArticle(${id}) 에러:`, error);
+    throw error;
+  }
+}

7f807a759c51b9a2db3c816e6832eb117e523b63
ydj-13 <de2013@naver.com>
2026-06-21 11:29:36 +0900

    feat:footer 작업

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,41 @@
+import Link from "next/link";
 import React from "react";
+import FacebookImg from "../../assets/ic_facebook.svg";
+import InstagramImg from "../../assets/ic_instagram.svg";
+import TwitterImg from "../../assets/ic_twitter.svg";
+import YoutubeImg from "../../assets/ic_youtube.svg";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div>바닥</div>;
+  return (
+    <footer className=" h-[10rem] py-[2rem] px-[12.5rem] items-start bg-[#111827]">
+      <div className="flex justify-between items-center self-stretch max-w-[120rem]">
+        <span className="font-pretendard text-[1rem] font-[400] leading-normal text-[#9CA3AF]">
+          ©codeit - 2024
+        </span>
+        <div className="flex items-start gap-[1.875rem]">
+          <span className="font-pretendard text-[1rem] font-[400] leading-normal text-[#E5E7EB]">
+            ©codeit - 2024
+          </span>
+          <span className="font-pretendard text-[1rem] font-[400] leading-normal text-[#E5E7EB]">
+            ©codeit - 2024
+          </span>
+        </div>
+        <div className="flex w-[7.25rem] items-start gap-[0.75rem]">
+          <Link href={"https://www.facebook.com"} target="_blank">
+            <Image src={FacebookImg} alt="페이스북 아이콘" />
+          </Link>
+          <Link href={"https://x.com/home"} target="_blank">
+            <Image src={TwitterImg} alt="트위터 아이콘" />
+          </Link>
+          <Link href={"https://www.youtube.com"} target="_blank">
+            <Image src={YoutubeImg} alt="유튜브 아이콘" />
+          </Link>
+          <Link href={"https://www.instagram.com"} target="_blank">
+            <Image src={InstagramImg} alt="인스타그램 아이콘" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }

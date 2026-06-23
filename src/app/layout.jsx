b8671d2875d861eddd1b842bdf28d5048abca1aa
apple <apple@appleui-MacBookAir.local>
2026-06-23 19:55:33 +0900

    초기세팅, tailwind 적용, layout.jsx, page.jsx, providers.jsx 파일 작업

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import Providers from "./providers";
+import "./global.css";
+
+export const metadata = {
+  title: "판다마켓",
+  description: "React Query로 구성한 판다마켓 중고 거래 서비스",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}

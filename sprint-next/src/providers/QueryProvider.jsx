bf6513a0f2d82cbad0abc2e293764f08e4d1d829
daeyeong1573 <rlaeodud9983@naver.com>
2026-06-29 00:41:39 +0900

    refact : 사용되지 않는 코드 변경

--- sprint-next/src/providers/QueryProvider.jsx
+++ sprint-next/src/providers/QueryProvider.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {
-  isServer,
   QueryCache,
   QueryClient,
   QueryClientProvider,
@@ -30,17 +29,14 @@ function makeQueryClient() {
   });
 }
 
-let browserQueryClient = undefined;
+let browserQueryClient;
 
 function getQueryClient() {
-  if (isServer) {
-    // 서버에서는 항상 새로운 쿼리 클라이언트를 만들어 반환
+  if (typeof window === "undefined") {
     return makeQueryClient();
-  } else {
-    // 브라우저에서는 이미 만들어진 쿼리 클라이언트를 반환
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
   }
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
 }
 
 export default function QueryProvider({ children }) {

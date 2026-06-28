3fe0498bd47a4ff745d192b10ba4c997c11c825b
damdadaxx <hyojudev@gmail.com>
2026-06-28 19:58:17 +0900

    refactor: 공통 컴포넌트 정리

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,12 +1,20 @@
-import { SOCIAL_CONFIG } from "@/lib/constants/constants";
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
+import { SOCIAL_CONFIG, WIDTH_HEADER_LIST } from "@/lib/constants/constants";
+
 export default function Footer() {
+  const pathname = usePathname();
+
+  if (WIDTH_HEADER_LIST.includes(pathname)) return null;
+
   return (
     <footer className='h-[160px]'>
       <div className='fixed bottom-0 left-0 right-0 z-footer bg-secondary-900'>
-        <div className="flex flex-wrap md:flex-nowrap justify-between gap-x-[20px] gap-y-[24px] md:gap-y-0 w-full h-[160px] max-w-[1200px] px-[16px] md:px-[24px] xl:px-[200px] py-[32px] mx-auto">
+        <div className='flex flex-wrap md:flex-nowrap justify-between gap-x-[20px] gap-y-[24px] md:gap-y-0 w-full h-[160px] max-w-[1200px] px-[16px] md:px-[24px] xl:px-[200px] py-[32px] mx-auto'>
           <p className='order-3 md:order-none w-full md:w-auto text-[16px] text-secondary-400'>
             &copy;codeit - 2024
           </p>
---
+++ src/components/common/Form/FormLabel.jsx
@@ -0,0 +1,14 @@
+import { clsx } from "clsx";
+
+export default function FormLabel({ label, size = "md" }) {
+  return (
+    <p
+      className={clsx(
+        "text-[18px]/[calc(26/18)] font-bold text-secondary-800",
+        size === "md" ? "mb-[16px]" : "mb-[8px] md:mb-[16px]",
+      )}
+    >
+      {label}
+    </p>
+  );
+}

f628d591f7fe2102b0e86b99052222340595cf9f
apple <apple@appleui-MacBookAir.local>
2026-08-05 15:34:35 +0900

    fix: 초기 세팅 및 app 폴더 작업

---
+++ src/app/board/page.tsx
@@ -0,0 +1,5 @@
+import FreeboardPage from "@/components/pages/FreeboardPage";
+
+export default function Board() {
+  return <FreeboardPage />;
+}
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,5 @@
+import SignInPage from "@/components/pages/SignInPage";
+
+export default function SignIn() {
+  return <SignInPage />;
+}
